296543d63f35ab9aafb8f03218fe5506620dda9e
Ali Emre Gülcü <aliemreglc@gmail.com>
2024-03-25 20:13:30 +0300

    Added Linux source directory to workspace and added module_params to hello module

--- hello/hello.c
+++ hello/hello.c
@@ -1,3 +1,4 @@
+#include "linux/moduleparam.h"
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
@@ -7,16 +8,19 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Lephar");
 MODULE_DESCRIPTION("Hello World Module");
 
+int hello_data = 3;
+module_param(hello_data, int, 0);
+
 static int __init hello_init(void)
 {
-    pr_info("Hello World.\n");
+    pr_info("Hello World %d.\n", hello_data);
 
     return 0;
 }
 
 static void __exit hello_cleanup(void)
 {
-    pr_info("Goodbye World.\n");
+    pr_info("Goodbye World %d.\n", hello_data);
 }
 
 module_init(hello_init);
